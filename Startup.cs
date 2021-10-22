df44d935d6f4ddc568fc96b12fe6d91cb5f2c559
John Winko <john.winko@gmail.com>
2021-10-21 21:08:56 -0500

    updated appsettings

--- Startup.cs
+++ Startup.cs
@@ -28,8 +28,11 @@ namespace MyBlog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // services.AddDbContext<ApplicationDbContext>(options =>
+            //     options.UseSqlServer(
+            //         Configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseNpgsql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
