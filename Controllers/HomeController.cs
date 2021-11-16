cc4cc1b92622970e2235668d5ef8facfff62ecd2
John Winko <john.winko@gmail.com>
2021-11-16 16:12:08 -0600

    Initial carve in from using the start bootstrap clean blog template

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,14 @@ namespace MyBlog.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
