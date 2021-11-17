5d653126e0b3ef670002b1ca006a9a5be7226b8d
John Winko <john.winko@gmail.com>
2021-11-16 18:12:18 -0600

    Created gmail app password and updated appsettings.json with details

---
+++ ViewModels/MailSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.ViewModels
+{
+    public class MailSettings
+    {
+        // so we can configure and use smtp server from google for example 
+        // qvsalsnwreohebsn
+        public string Mail { get; set; }
+        public string DisplayName { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+    }
+}
