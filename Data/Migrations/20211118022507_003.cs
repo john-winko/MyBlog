6e6d0a77aa6039be90086d381e5814cd1c8cf9c8
John Winko <john.winko@gmail.com>
2021-11-17 20:37:52 -0600

    updated database/models to match for blog user per the video and tweaked the registration pages

---
+++ Data/Migrations/20211118022507_003.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyBlog.Data.Migrations
+{
+    /// <summary>
+    /// Updated BlogUser to match what was in video
+    /// </summary>
+    public partial class _003 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "AspNetUsers");
+        }
+    }
+}
--- Models/BlogUser.cs
+++ Models/BlogUser.cs
@@ -17,6 +17,11 @@ namespace MyBlog.Models
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Display Name")]
+        public string DisplayName { get; set; }
+
         public byte[] ImageData { get; set; }
         public string ContentType { get; set; }
 
