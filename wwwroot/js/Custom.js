2070b2e23579b825b36cbc91b2a30e1128937ce9
John Winko <john.winko@gmail.com>
2021-11-21 16:44:38 -0600

    Basic error messages added for tags

--- wwwroot/js/Custom.js
+++ wwwroot/js/Custom.js
@@ -4,9 +4,24 @@ function AddTag() {
     // get a reference to the TagEntry input element (above the add/delete buttons)
     var tagEntry = document.getElementById("TagEntry");
 
-    // Create a new select option
-    let newOption = new Option(tagEntry.value, tagEntry.value);
-    document.getElementById("TagList").options[index++] = newOption;
+    // lets use the new search function to help detect an error state
+    let searchResult = search(tagEntry.value);
+    if (searchResult != null) {
+        // trigger sweet alert for dup issue
+        // TODO: change this to a validate tag as bool return
+        Swal.fire({
+            title: "Error!",
+            text: "Do you want to continue",
+            icon: "error",
+            confirmButtonText: "Cool"
+        });
+    } else {
+
+        // Create a new select option
+        let newOption = new Option(tagEntry.value, tagEntry.value);
+        document.getElementById("TagList").options[index++] = newOption;
+
+    }
 
     // Clear out TagEntry control
     tagEntry.value = "";
@@ -30,7 +45,7 @@ function DeleteTag() {
     }
 }
 
-$("form").on("submit", function() {
+$("form").on("submit", function () {
     $("#TagList option").prop("selected", "selected");
 });
 
@@ -47,4 +62,23 @@ if (tagValues != "") {
 function ReplaceTag(tag, index) {
     let newOption = new Option(tag, tag);
     document.getElementById("TagList").options[index] = newOption;
+}
+
+// the search function will detect empty or duplicate tag
+// and return an error string if an error is detected
+function search(str) {
+    if (str == "") {
+        return "Empty tags are not permitted";
+    }
+
+    var tagsEl = document.getElementById("TagList");
+    if (tagsEl) {
+        let options = tagsEl.options;
+        for (let index = 0; index < options.length; index++) {
+            if (options[index].value == str) {
+                return `The Tag #${str} was detected as a duplicate not permitted`;
+            }
+        }
+    }
+    return null;
 }
